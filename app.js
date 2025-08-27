f47b9704c79e6598340649874ab1d87a81fbcb4a
Yaser Shaikh <syaser327@gmail.com>
2025-08-27 20:56:02 +0530

    Updated.

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on http://0.0.0.0:3000");
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
+
 
