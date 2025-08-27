67d97267b74fab92525221a92db8267ad72f9fb2
Yaser Shaikh <syaser327@gmail.com>
2025-08-27 20:37:49 +0530

    Updated.

--- node-docker-demo/app.js
+++ node-docker-demo/app.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on http://0.0.0.0:3000");
 });
+
